9f6c9b0b630097f5b9c9852403e93771d2d9bf17
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-09-01 17:35:30 +0300

    Initial

---
+++ src/main/java/com/nouhoun/springboot/jwt/integration/repository/RandomCityRepository.java
@@ -0,0 +1,7 @@
+package com.nouhoun.springboot.jwt.integration.repository;
+
+import com.nouhoun.springboot.jwt.integration.domain.RandomCity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RandomCityRepository extends CrudRepository<RandomCity, Long> {
+}
---
+++ src/main/java/com/nouhoun/springboot/jwt/integration/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nouhoun.springboot.jwt.integration.repository;
+
+import com.nouhoun.springboot.jwt.integration.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
